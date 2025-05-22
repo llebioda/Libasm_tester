5ac9736257f4250d28f69fd8a24307d8b1bcc61e
Lowan Lebioda-Leger <llebioda@student.42.fr>
2025-05-22 12:02:20 +0200

    added errno check

--- read_test.c
+++ read_test.c
@@ -2,10 +2,10 @@
 
 extern ssize_t ft_read(int fd, void *buf, size_t count);
 
-static t_bool read_is_valid(int fd, const char *expected, size_t count,
+static t_bool read_is_valid(const int fd, const char *expected, const size_t count,
     void *std_buffer, void *ft_buffer,
     const ssize_t res, const ssize_t ft_res,
-    const int res_errno, const int ft_res_errno)
+    const int res_errno, const int ft_res_errno, const int errno_value)
 {
     if (res != ft_res)
     {
@@ -14,11 +14,25 @@ static t_bool read_is_valid(int fd, const char *expected, size_t count,
         return FALSE;
     }
 
-    if (res < 0 && res_errno != ft_res_errno)
+    if (res < 0)
     {
-        printf(RED "[READ] Errno value mismatch on fd=%d with count=%zu (%s): %d != %d" RESET "\n",
-            fd, count, expected, res_errno, ft_res_errno);
-        return FALSE;
+        // Verify expected errno value
+        if (errno_value != res_errno)
+        {
+            printf(RED "[READ] " RESET RED_ "** TEST ERROR **" RESET RED
+                "\nErrno value mismatch on fd=%d with count=%zu (%s)\nexpected : %d\ngot > std: %d\n    > ft : %d" RESET "\n",
+                fd, count, expected, errno_value, res_errno, ft_res_errno);
+            return FALSE;
+        }
+
+        if (res_errno != ft_res_errno)
+        {
+            printf(RED "[READ] Errno value mismatch on fd=%d with count=%zu (%s): %d != %d" RESET "\n",
+                fd, count, expected, res_errno, ft_res_errno);
+            return FALSE;
+        }
+
+        return TRUE;
     }
 
     // Prevent segfault if printed
@@ -45,7 +59,7 @@ static t_bool read_is_valid(int fd, const char *expected, size_t count,
     return result;
 }
 
-static void read_test(int fd, const char *expected, size_t count)
+static void read_test(int fd, const char *expected, size_t count, const int errno_value)
 {
     void *std_buffer = malloc(count + 1);
     void *ft_buffer = malloc(count + 1);
@@ -65,7 +79,7 @@ static void read_test(int fd, const char *expected, size_t count)
     int ft_res_errno = errno;
 
     read_is_valid(fd, expected, count, std_buffer, ft_buffer,
-                    res, ft_res, res_errno, ft_res_errno);
+                    res, ft_res, res_errno, ft_res_errno, errno_value);
     free(std_buffer);
     free(ft_buffer);
 }
@@ -76,7 +90,7 @@ static void fill_with_random_values(unsigned char *buffer, size_t size)
         buffer[i] = rand() % 256; // Generate a random byte (0-255)
 }
 
-static void read_benchmark(int fd, const char *expected, size_t count)
+static void read_benchmark(int fd, const char *expected, size_t count, const int errno_value)
 {
 
     void *std_buffer = malloc(count + 1);
@@ -110,7 +124,7 @@ static void read_benchmark(int fd, const char *expected, size_t count)
         time_ft += ((double)(end - start)) / 1000;
 
         if (!read_is_valid(fd, expected, count, std_buffer, ft_buffer,
-                            res, ft_res, res_errno, ft_res_errno))
+                            res, ft_res, res_errno, ft_res_errno, errno_value))
         {
             printf(YELLOW "Benchmark cancelled" RESET "\n");
             free(std_buffer);
@@ -216,35 +230,49 @@ void read_tester(void)
         return;
     }
 
-    read_test(fd3, "Short test", 10);
-    read_test(fd4, "A bit longer string for testing.", 32);
-    read_test(fd5, "empty string.", 0);
-    read_test(fd6, "Testing special characters: !@#$%^&*()_+-=", 30);
-    read_test(fd7, "Very large input: ......................................................................................................................................................................................................................", 200);
+    read_test(fd3, "Short test", 10, 0);
+    read_test(fd4, "A bit longer string for testing.", 32, 0);
+    read_test(fd5, "empty string.", 0, 0);
+    read_test(fd6, "Testing special characters: !@#$%^&*()_+-=", 30, 0);
+    read_test(fd7, "Very large input: ......................................................................................................................................................................................................................", 200, 0);
 
-    read_test(fd3, "First write.", 12);
-    read_test(fd3, "Second write!", 13);
-    read_test(fd3, "Final write.", 12);
-    read_test(fd4, B_1_000"AA_-", 1003);
-    read_test(fd4, A_10_000"B"A_10_000, 15000);
+    read_test(fd3, "First write.", 12, 0);
+    read_test(fd3, "Second write!", 13, 0);
+    read_test(fd3, "Final write.", 12, 0);
+    read_test(fd4, B_1_000"AA_-", 1003, 0);
+    read_test(fd4, A_10_000"B"A_10_000, 15000, 0);
 
-    read_test(fd4, "Unicode test: 日本語, русский, عربى", 30);
+    read_test(fd4, "Unicode test: 日本語, русский, عربى", 30, 0);
 
-    read_test(fd5, "Line 1\nLine 2\nLine 3", 18);
-    read_test(fd5, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n", 14);
-    read_test(fd5, "\t\t\t\v\v\v\n\n\n\t\b\n\t\t\v\n\a\b\b", 19);
+    read_test(fd5, "Line 1\nLine 2\nLine 3", 18, 0);
+    read_test(fd5, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n", 14, 0);
+    read_test(fd5, "\t\t\t\v\v\v\n\n\n\t\b\n\t\t\v\n\a\b\b", 19, 0);
 
-    read_test(fd3, "This should not be written", 0);
-    read_test(fd6, "NULL1_NULL2_NULL3", 10);
-    read_test(fd6, "NULL1_NULL2_NULL3", 0);
+    read_test(fd3, "This should not be written", 0, 0);
+    read_test(fd6, "NULL1_NULL2_NULL3", 10, 0);
+    read_test(fd6, "NULL1_NULL2_NULL3", 0, 0);
 
-    read_test(fd7, "Partial write test!", 10);
+    read_test(fd7, "Partial write test!", 10, 0);
 
     close(fd3);
     close(fd4);
     close(fd5);
     close(fd6);
     close(fd7);
+
+    read_test(-1, "fd -1", 0, EBADF);
+    read_test(-2, "fd -2", 0, EBADF);
+    read_test(10, "fd 10", 0, EBADF);
+    read_test(12345, "fd 12345", 0, EBADF);
+    read_test(42424242, "fd 42424242", 0, EBADF);
+    read_test(2147483647, "fd 2147483647", 0, EBADF);
+
+    fd3 = open(TMP_DIR "/test1", O_WRONLY | O_APPEND, 0644);
+    read_test(fd3, "couldn't write in", 0, EBADF);
+    read_test(fd3, "couldn't write in for 2 time", 0, EBADF);
+
+    close(fd3);
+    read_test(fd3, "file is closed", 0, EBADF);
 
     /* BENCHMARK */
     // Prepare data to read in test files
@@ -278,8 +306,9 @@ void read_tester(void)
         return;
     }
 
-    read_benchmark(fd3, "", 0);
-    read_benchmark(fd3, "", 1);
-    read_benchmark(fd4, "Hello World!", 12);
-    read_benchmark(fd5, A_1_000".\n", 1002);
+    read_benchmark(fd3, "", 0, 0);
+    read_benchmark(fd3, "", 1, 0);
+    read_benchmark(fd4, "Hello World!", 12, 0);
+    read_benchmark(fd5, A_1_000".\n", 1002, 0);
+    read_benchmark(-1, "fd -1", 0, EBADF);
 }
--- write_test.c
+++ write_test.c
@@ -21,9 +21,9 @@ static char *get_writable_buf(const void *buf, size_t count)
     return dst;
 }
 
-static t_bool write_is_valid(int fd, const void *buf, size_t count,
+static t_bool write_is_valid(const int fd, const void *buf, const size_t count,
     const ssize_t res, const ssize_t ft_res,
-    const int res_errno, const int ft_res_errno)
+    const int res_errno, const int ft_res_errno, const int errno_value)
 {
     char *tmp_buf = NULL;
 
@@ -36,13 +36,29 @@ static t_bool write_is_valid(int fd, const void *buf, size_t count,
         return FALSE;
     }
 
-    if (res < 0 && res_errno != ft_res_errno)
+    if (res < 0)
     {
-        tmp_buf = get_writable_buf(buf, count);
-        printf(RED "[WRITE] Errno value mismatch on fd=%d with count=%zu (%s): %d != %d" RESET "\n",
-            fd, count, tmp_buf, res_errno, ft_res_errno);
-        free(tmp_buf);
-        return FALSE;
+        // Verify expected errno value
+        if (errno_value != res_errno)
+        {
+            tmp_buf = get_writable_buf(buf, count);
+            printf(RED "[WRITE] " RESET RED_ "** TEST ERROR **" RESET RED
+                "\nErrno value mismatch on fd=%d with count=%zu (%s)\nexpected : %d\ngot > std: %d\n    > ft : %d" RESET "\n",
+                fd, count, tmp_buf, errno_value, res_errno, ft_res_errno);
+            free(tmp_buf);
+            return FALSE;
+        }
+
+        if (res_errno != ft_res_errno)
+        {
+            tmp_buf = get_writable_buf(buf, count);
+            printf(RED "[WRITE] Errno value mismatch on fd=%d with count=%zu (%s): %d != %d" RESET "\n",
+                fd, count, tmp_buf, res_errno, ft_res_errno);
+            free(tmp_buf);
+            return FALSE;
+        }
+
+        return TRUE;
     }
 
     void *std_buffer = malloc(count + 1);
@@ -100,17 +116,18 @@ static t_bool write_is_valid(int fd, const void *buf, size_t count,
     return result;
 }
 
-static void write_test(int fd, const void *buf, size_t count)
+static void write_test(int fd, const void *buf, size_t count, const int errno_value)
 {
     ssize_t res = write(fd, buf, count);
     int res_errno = errno;
     ssize_t ft_res = ft_write(fd, buf, count);
     int ft_res_errno = errno;
 
-    write_is_valid(fd, buf, count, res, ft_res, res_errno, ft_res_errno);
+    write_is_valid(fd, buf, count, res, ft_res,
+        res_errno, ft_res_errno, errno_value);
 }
 
-static void write_benchmark(int fd, const void *buf, size_t count)
+static void write_benchmark(int fd, const void *buf, size_t count, const int errno_value)
 {
     clock_t start, end;
     double time_lib = 0.0, time_ft = 0.0, efficiency;
@@ -129,7 +146,8 @@ static void write_benchmark(int fd, const void *buf, size_t count)
         int ft_res_errno = errno;
         time_ft += ((double)(end - start)) / 1000;
 
-        if (!write_is_valid(fd, buf, count, res, ft_res, res_errno, ft_res_errno))
+        if (!write_is_valid(fd, buf, count, res, ft_res,
+                res_errno, ft_res_errno, errno_value))
         {
             printf(YELLOW "Benchmark cancelled" RESET "\n");
             return;
@@ -161,29 +179,29 @@ void write_tester(void)
         return;
     }
 
-    write_test(fd3, "Short test", 10);
-    write_test(fd4, "A bit longer string for testing.", 32);
-    write_test(fd5, "empty string.", 0);
-    write_test(fd6, "Testing special characters: !@#$%^&*()_+-=", 30);
-    write_test(fd7, "Very large input: ......................................................................................................................................................................................................................", 200);
+    write_test(fd3, "Short test", 10, 0);
+    write_test(fd4, "A bit longer string for testing.", 32, 0);
+    write_test(fd5, "empty string.", 0, 0);
+    write_test(fd6, "Testing special characters: !@#$%^&*()_+-=", 30, 0);
+    write_test(fd7, "Very large input: ......................................................................................................................................................................................................................", 200, 0);
 
-    write_test(fd3, "First write.", 12);
-    write_test(fd3, "Second write!", 13);
-    write_test(fd3, "Final write.", 12);
-    write_test(fd4, B_1_000"AA_-", 1003);
-    write_test(fd4, A_10_000"B"A_10_000, 15000);
+    write_test(fd3, "First write.", 12, 0);
+    write_test(fd3, "Second write!", 13, 0);
+    write_test(fd3, "Final write.", 12, 0);
+    write_test(fd4, B_1_000"AA_-", 1003, 0);
+    write_test(fd4, A_10_000"B"A_10_000, 15000, 0);
 
-    write_test(fd4, "Unicode test: 日本語, русский, عربى", 30);
+    write_test(fd4, "Unicode test: 日本語, русский, عربى", 30, 0);
 
-    write_test(fd5, "Line 1\nLine 2\nLine 3", 18);
-    write_test(fd5, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n", 14);
-    write_test(fd5, "\t\t\t\v\v\v\n\n\n\t\b\n\t\t\v\n\a\b\b", 19);
+    write_test(fd5, "Line 1\nLine 2\nLine 3", 18, 0);
+    write_test(fd5, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n", 14, 0);
+    write_test(fd5, "\t\t\t\v\v\v\n\n\n\t\b\n\t\t\v\n\a\b\b", 19, 0);
 
-    write_test(fd3, "This should not be written", 0);
-    //write_test(fd6, NULL, 10);
-    write_test(fd6, NULL, 0);
+    write_test(fd3, "This should not be written", 0, 0);
+    write_test(fd6, NULL, 10, EFAULT);
+    write_test(fd6, NULL, 0, 0);
 
-    write_test(fd7, "Partial write test!", 10);
+    write_test(fd7, "Partial write test!", 10, 0);
 
     close(fd3);
     close(fd4);
@@ -191,21 +209,36 @@ void write_tester(void)
     close(fd6);
     close(fd7);
 
+    write_test(-1, "fd -1", 6, EBADF);
+    write_test(-2, "fd -2", 6, EBADF);
+    write_test(10, "fd 10", 6, EBADF);
+    write_test(12345, "fd 12345", 9, EBADF);
+    write_test(42424242, "fd 42424242", 12, EBADF);
+    write_test(2147483647, "fd 2147483647", 14, EBADF);
+
+    fd3 = open(TMP_DIR "/test1", O_RDONLY, 0644);
+    write_test(fd3, "couldn't write in", 18, EBADF);
+    write_test(fd3, "couldn't write in for 2 time", 29, EBADF);
+
+    close(fd3);
+    write_test(fd3, "file is closed", 15, EBADF);
+
     /* BENCHMARK */
     fd3 = open(TMP_DIR "/benchmark1", O_RDWR | O_CREAT | O_TRUNC, 0644);
     fd4 = open(TMP_DIR "/benchmark2", O_RDWR | O_CREAT | O_TRUNC, 0644);
     fd5 = open(TMP_DIR "/benchmark3", O_RDWR | O_CREAT | O_TRUNC, 0644);
 
-    if (fd3 < 0 | fd4 < 0 | fd5 < 0) 
+    if (fd3 < 0 | fd4 < 0 | fd5 < 0)
     {
         printf(RED "[WRITE] ERROR Failed to open benchmark files!" RESET "\n");
         return;
     }
 
-    write_benchmark(fd3, "", 0);
-    write_benchmark(fd3, "", 1);
-    write_benchmark(fd4, "Hello World!", 12);
-    write_benchmark(fd5, A_1_000".\n", 1002);
+    write_benchmark(fd3, "", 0, 0);
+    write_benchmark(fd3, "", 1, 0);
+    write_benchmark(fd4, "Hello World!", 12, 0);
+    write_benchmark(fd5, A_1_000".\n", 1002, 0);
+    write_benchmark(-1, NULL, 0, EBADF);
 
     close(fd3);
     close(fd4);
